bc5d15b7a3dab1b6c701ae1c26b05bf57eec1bd5
rahulvraut9 <rahulvraut9@gmail.com>
2023-08-13 00:56:06 +0530

    06 Factorial task
    06 Factorial Task

--- 06_factorialNo.js
+++ 06_factorialNo.js
@@ -20,3 +20,29 @@ factorial(9);
 factorial(11);
 factorial(7);
 
+//recursion
+
+// function factoral(num){
+//     var res = num * factoral(num-1) //recursion example
+var factres =1;
+// } 
+function fact(num){
+    if(num==2){
+        console.log(factres);  
+    }else{
+         factres = num * fact(num-1);
+    }
+}
+fact(5)
+
+function factoril(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factoril(n - 1);
+    }
+}
+
+const number = 5; // Change this number to calculate the factorial of a different value
+const resut = factoril(number);
+console.log(`Factorial of ${number} is ${resut}`);
