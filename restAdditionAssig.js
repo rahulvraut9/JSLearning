96a38f58999ff605c4a7617102732e2c711831c2
rahulvraut9 <rahulvraut9@gmail.com>
2023-08-22 00:29:26 +0530

    restAddition Task
    restAddition Task

---
+++ restAdditionAssig.js
@@ -0,0 +1,15 @@
+function add(num1,num2,...num){
+    sum = num1+num2;
+    string = ""
+    for(i=0;i<=num.length-1;i++){
+    sum+=num[i];
+    string = string + ","+ num[i] 
+    
+   }
+   console.log(`The Addition of ${num1}, ${num2} ${string} is ${sum}`);
+   
+    }
+    add(5,6);
+    add(9,5,6);
+    add(70,90,51,68);
+    add(89,90,45,78,90,51,68);
