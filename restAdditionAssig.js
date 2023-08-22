301b28d435f4bc4f5c93c295a976c50e27486605
rahulvraut9 <rahulvraut9@gmail.com>
2023-08-22 17:28:02 +0530

    change
    change

--- restAdditionAssig.js
+++ restAdditionAssig.js
@@ -4,8 +4,7 @@ function add(num1,num2,...num){
     for(i=0;i<=num.length-1;i++){
     sum+=num[i];
     string = string + ","+ num[i] 
-    
-   }
+       }
    console.log(`The Addition of ${num1}, ${num2} ${string} is ${sum}`);
    
     }
@@ -13,3 +12,5 @@ function add(num1,num2,...num){
     add(9,5,6);
     add(70,90,51,68);
     add(89,90,45,78,90,51,68);
+
+    
