4dd0f8e58297a8f4feae8fb0d8f9ebc975774cc4
rahulvraut9 <rahulvraut9@gmail.com>
2023-09-21 01:09:35 +0530

    DOM assigmnet
    DOM Assigmnet

---
+++ 27_domAssigment.js
@@ -0,0 +1,6 @@
+const mainHeading = document.getElementById("mainHeading");
+console.log(mainHeading);
+const secondAdvantage = document.getElementById("secondAdvantage");
+console.log(secondAdvantage);
+const advDiv = document.getElementById("advantages");
+console.log(advDiv);
